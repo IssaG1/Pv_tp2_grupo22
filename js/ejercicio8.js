31d92374a1019f1390bfdf680932e3b8821e7791
Misael3101 <torres.31012407@gmail.com>
2025-04-23 17:14:38 -0300

    8

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -8,16 +8,17 @@ Invocar la función y enviar los argumentos con diferentes números para probar.
  */
 
 
-let numero1= 40
-let numero2= 30
+let numero1 = 40;
+let numero2 = 30;
 
-function calcularMayor(numero1,numero2){
+const calcularMayor = (numero1, numero2) => {
     if (numero1 > numero2) {
         alert("el numero mayor es " + numero1);
     } else if (numero1 < numero2) {
         alert("el numero mayor es " + numero2);
-    } else if (numero1 == numero2) {
+    } else {
         alert("los numeros son iguales " + numero1 + " y " + numero2);
     }
-}
-calcularMayor(55,60);
+};
+
+calcularMayor(55, 60);
