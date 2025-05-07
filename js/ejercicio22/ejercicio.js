c3352e45701e16b6e34592937fb5034b7516b091
MaximilianoT13 <134133637+MaximilianoT13@users.noreply.github.com>
2025-05-07 18:45:28 -0300

    Revert "agregué el ejercicio 22"

--- js/ejercicio22/ejercicio.js
+++
@@ -1,6 +0,0 @@
-import { agregarTexto } from './funcion.js';
-let texto=document.querySelector('#texto');
-let p=document.getElementById("parrafo");
-texto.addEventListener("input",()=>{
-    agregarTexto(p,texto);
-})
